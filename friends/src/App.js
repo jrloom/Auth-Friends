babac57d541b88d42ff42f9f72f0e8aca303476c
Jason Loomis <jason@jrloomis.com>
2019-10-04 01:47:22 -0400

    route to login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>nothing to see here</div>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+      <Route path="/login" component={Login} />
     </div>
   );
 }
