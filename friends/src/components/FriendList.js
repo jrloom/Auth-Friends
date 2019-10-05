afad8cba9f2e1b5553c26f88ec26832e845ef361
Jason Loomis <jason@jrloomis.com>
2019-10-05 14:09:09 -0400

    import addfriend

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import AddFriend from "./AddFriend";
 import Friend from "./Friend";
 
 const FriendList = () => {
@@ -13,15 +14,18 @@ const FriendList = () => {
   });
 
   return (
-    <div className="friends">
-      <h1 className="friends__title">You have friends!</h1>
-      <ul className="friends__list">
-        {friends.map(friend => (
-          <li key={friend.id} className="friends__friend">
-            <Friend friend={friend} />
-          </li>
-        ))}
-      </ul>
+    <div className="allthefriends">
+      <AddFriend />
+      <div className="friends">
+        <h1 className="friends__title">You have friends!</h1>
+        <ul className="friends__list">
+          {friends.map(friend => (
+            <li key={friend.id} className="friends__friend">
+              <Friend friend={friend} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
